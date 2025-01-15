671a6d501c88dc5a182533b8354165e177dcf83a
hiromu47 <hirock358@gmail.com>
2025-01-16 04:52:34 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'prompt-dictionary-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/script.js',
+    '/icons/icon-192x192.svg',
+    '/icons/icon-512x512.svg'
+];
+
+// インストール時にキャッシュ
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// キャッシュを使用
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// 古いキャッシュの削除
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
